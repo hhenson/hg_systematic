39f9ddb6552f9d4dbe79a7ffee462545eb13d20a
Howard Henson <howard@henson.me.uk>
2024-11-01 22:29:09 +0400

    Add first pass auto regressive generator

--- src/hg_systematic/data/generators.py
+++ src/hg_systematic/data/generators.py
@@ -4,7 +4,8 @@ from zoneinfo import ZoneInfo
 
 import numpy as np
 from hg_oap.dates.dt_utils import date_tz_to_utc
-from hgraph import generator, TS, EvaluationEngineApi
+from hgraph import generator, TS, EvaluationEngineApi, graph, lag, delayed_binding, feedback, compute_node, \
+    RECORDABLE_STATE, TimeSeriesSchema
 
 
 @generator
@@ -27,3 +28,31 @@ def white_noise_generator(
     while current <= end:
         yield current, float(next(samples))
         current += frequency
+
+
+class ARState(TimeSeriesSchema):
+    previous_terms: TS[tuple[float,...]]
+
+@compute_node
+def auto_regressive_generator(
+        white_noise: TS[float],
+        order: int = 1,
+        initial_values: tuple[float, ...] = (1.0,),
+        coefficients: tuple[float, ...] = (1.0, 0.5),
+        _state: RECORDABLE_STATE[ARState] = None
+) -> TS[float]:
+    """
+    An autoregressive generator.
+    The order defines how many terms to use.
+    The size of the initial values is ``order`` and coefficients must be the size of ``order+1``.
+    """
+    result = white_noise.value + coefficients[0]
+    prev = _state.previous_terms.value
+    result += sum(coefficients[i+1] * prev[i] for i in range(order))
+    _state.previous_terms.apply_result((result,) + prev[1:])
+    return result
+
+@auto_regressive_generator.start
+def autoregressive_generator_start(initial_values: tuple[float, ...], _state: RECORDABLE_STATE[ARState] = None):
+    _state.previous_terms.apply_result(initial_values)
+
--- tests/data/test_generators.py
+++ tests/data/test_generators.py
@@ -1,11 +1,22 @@
-from hgraph import MIN_ST, MIN_TD
+from hgraph import MIN_ST, MIN_TD, TS, graph
 from hgraph.test import eval_node
 
-from hg_systematic.data.generators import white_noise_generator
+from hg_systematic.data.generators import white_noise_generator, auto_regressive_generator
 
 
 def test_white_noise_generator():
+    results = eval_node(white_noise_generator, frequency=MIN_TD, __end_time__=MIN_ST + MIN_TD * 5)
+    print(results)
+    assert len(results) == 5
 
-    results = eval_node(white_noise_generator, frequency=MIN_TD, __end_time__=MIN_ST + MIN_TD*5)
+
+def test_auto_regressive_generator():
+
+    @graph
+    def g() -> TS[float]:
+        noise = white_noise_generator(frequency=MIN_TD)
+        return auto_regressive_generator(noise)
+
+    results = eval_node(g, __end_time__=MIN_ST + MIN_TD * 5)
     print(results)
     assert len(results) == 5
