1864620a3fd6564bb36e34ce82640e34d38662ea
Howard Henson <howard@henson.me.uk>
2025-02-04 21:20:38 +0400

    Update to move BCOM specific logic out into examples, will commit examples once the logic looks a bit better.

--- src/hg_systematic/operators/_index.py
+++ src/hg_systematic/operators/_index.py
@@ -2,14 +2,13 @@ from datetime import date
 from enum import Enum
 
 from hgraph import TSD, TS, subscription_service, TSS, TSL, Size, operator, SIZE, graph, map_, \
-    convert, switch_, if_then_else, sum_, merge, unpartition, index_of, const, DEFAULT, clone_type_var, \
-    TIME_SERIES_TYPE, month_of_year, year, format_, mul_, pass_through
+    convert, switch_, if_then_else, sum_, merge, unpartition, DEFAULT, clone_type_var, \
+    TIME_SERIES_TYPE, mul_, pass_through
 
-from hg_systematic.operators._calendar import calendar_for, HolidayCalendar, business_days, \
-    Periods, month_code
+from hg_systematic.operators._calendar import calendar_for, HolidayCalendar
 
 __all__ = ["INDEX_WEIGHTS", "index_composition", "index_assets", "index_rolling_contracts", "index_rolling_weights",
-           "index_weights", "DEFAULT_INDEX_PATH"]
+           "index_weights", "DEFAULT_INDEX_PATH", "ContractMatchState",]
 
 INDEX_WEIGHTS = TSD[str, TS[float]]
 
@@ -111,7 +110,7 @@ def index_weights(symbol: TS[str], dt: TS[date], rolling_rule: str,
     return weights
 
 
-class _ContractState(Enum):
+class ContractMatchState(Enum):
     ALL_KEYS_MATCH = 1
     LEFT_ONLY = 2
     RIGHT_ONLY = 3
@@ -127,16 +126,16 @@ def _to_tsd(keys: TSL[TS[str], SIZE], values: TSL[TS[float], SIZE]) -> TSD[str, 
     match = keys[0] == keys[1]
     lhs = values[1] == 0.0
     rhs = values[0] == 0.0
-    match_case = if_then_else(match, _ContractState.ALL_KEYS_MATCH,
-                     if_then_else(lhs, _ContractState.LEFT_ONLY,
-                                  if_then_else(rhs, _ContractState.RIGHT_ONLY, _ContractState.OTHERWISE)))
+    match_case = if_then_else(match, ContractMatchState.ALL_KEYS_MATCH,
+                              if_then_else(lhs, ContractMatchState.LEFT_ONLY,
+                                           if_then_else(rhs, ContractMatchState.RIGHT_ONLY, ContractMatchState.OTHERWISE)))
     out = switch_(
         {
-            _ContractState.ALL_KEYS_MATCH: lambda k, v: convert[TSD](k[0], sum_(v, 0.0)),
-            _ContractState.LEFT_ONLY: lambda k, v: convert[TSD](k[0], v[0]),
-            _ContractState.RIGHT_ONLY: lambda k, v: convert[TSD](k[1], v[1]),
-            _ContractState.OTHERWISE: lambda k, v: merge(convert[TSD](k[0], v[0]), convert[TSD](k[1], v[1]),
-                                                         disjoint=True),
+            ContractMatchState.ALL_KEYS_MATCH: lambda k, v: convert[TSD](k[0], sum_(v, 0.0)),
+            ContractMatchState.LEFT_ONLY: lambda k, v: convert[TSD](k[0], v[0]),
+            ContractMatchState.RIGHT_ONLY: lambda k, v: convert[TSD](k[1], v[1]),
+            ContractMatchState.OTHERWISE: lambda k, v: merge(convert[TSD](k[0], v[0]), convert[TSD](k[1], v[1]),
+                                                             disjoint=True),
         },
         match_case,
         keys,
@@ -145,58 +144,3 @@ def _to_tsd(keys: TSL[TS[str], SIZE], values: TSL[TS[float], SIZE]) -> TSD[str, 
     return out
 
 
-@graph(overloads=index_rolling_weights, requires=lambda m, s: m[SIZE].py_type == Size[2] and s["rule"] == "BCOM")
-def _bcom_rolling_weights(
-        rule: str,
-        calendar: HolidayCalendar,
-        dt: TS[date],
-        sz: SIZE = Size[2]
-) -> TSL[TS[float], Size[2]]:
-    days_of_month = business_days(Periods.Month, calendar, dt)
-    day_index = index_of(days_of_month, dt) + 1
-    rule_state = if_then_else(
-        day_index < 6,
-        _ContractState.LEFT_ONLY,
-        if_then_else(
-            day_index > 10,
-            _ContractState.RIGHT_ONLY,
-            _ContractState.OTHERWISE
-        )
-    )
-
-    return switch_(
-        {
-            _ContractState.LEFT_ONLY: lambda ndx: const((1.0, 0.0), TSL[TS[float], Size[2]]),
-            _ContractState.OTHERWISE: lambda ndx: TSL[TS[float], Size[2]].from_ts((10 - ndx) * .2, (ndx - 5) * .2),
-            _ContractState.RIGHT_ONLY: lambda ndx: const((0.0, 1.0), TSL[TS[float], Size[2]]),
-        },
-        rule_state,
-        day_index
-    )
-
-
-@graph(overloads=index_rolling_contracts, requires=lambda m, s: m[SIZE].py_type == Size[2] and s["rule"] == "BCOM")
-def _bcom_rolling_contracts(
-        rule: str,
-        symbol: TS[str],
-        dt: TS[date],
-        sz: SIZE = Size[2]
-) -> TSL[TS[str], Size[2]]:
-    roll_schedule = index_rolling_schedule("BCOM Index")[symbol]
-    m = month_of_year(dt)
-    y = year(dt)
-    # The roll from month and year
-    r1_m = roll_schedule[m][0]
-    r1_y = y + roll_schedule[m][1]
-    # The next month and year for given date
-    m1 = (m % 12) + 1
-    y1 = if_then_else(m1 == 0, y + 1, y)
-    # The roll to month and year
-    r2_m = roll_schedule[m1][0]
-    r2_y = y1 + roll_schedule[m1][1]
-    # Now we make a weak assumption that the contract format takes the form of:
-    # <key><month in letter><2 digit year> Comdty
-    return TSL[TS[str], Size[2]].from_ts(
-        format_("{}{}{:02} Comdty", symbol, month_code(r1_m), r1_y % 100),
-        format_("{}{}{:02} Comdty", symbol, month_code(r2_m), r2_y % 100)
-    )
